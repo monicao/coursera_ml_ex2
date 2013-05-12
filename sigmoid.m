78222dcecba638c720a6406fade54a16f9314959
Monica Olinescu <monica.olinescu@gmail.com>
2013-05-12 15:17:49 -0400

    Implementing the sigmoid function

--- sigmoid.m
+++ sigmoid.m
@@ -9,10 +9,26 @@ g = zeros(size(z));
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
 
+if size(z) == [1, 1]
+  g = 1 / (1 + e^-z);
+elseif size(z)(1) == 1 && size(z)(2) > 1
+  g = [];
+  for val = z
+    g(length(g) + 1) = 1 / (1 + e^-val);
+  endfor
 
-
-
+else 
+  g = [];
+  for r = 1:size(z)(1)
+    for c = 1:size(z)(2)
+      g(r, c) = 1 / (1 + e^-z(r, c));
+    endfor
+  endfor
+end
 
 % =============================================================
 
 end
+
+
+
