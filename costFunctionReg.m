bf7a2fb85604b07567cff0999551accd6812dd0e
Monica Olinescu <monica.olinescu@gmail.com>
2013-05-25 12:48:58 -0400

    Computing the regularized cost function

--- costFunctionReg.m
+++ costFunctionReg.m
@@ -17,9 +17,42 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+n = size(X)(2);
 
 
+% Compute the cost function
 
+for i = 1:m
+  h = sigmoid(X(i, :) * theta);
+  J = J + (-y(i) * log(h) - (1 - y(i)) * log(1 - h));
+endfor
+
+J    = 1 / m * J;
+
+lambda_term = 0;
+for j = 2:n
+  lambda_term = lambda_term + theta(j) ^ 2;
+end
+lambda_term = lambda / (2 * m) * lambda_term;
+
+J = J + lambda_term;
+
+% Compute the gradient
+
+for j = 2:n
+  for i = 1:m
+    h = sigmoid(X(i, :) * theta);
+    grad(j) = grad(j) + (h - y(i)) * X(i, j);
+  endfor
+  grad(j) = 1 / m * grad(j);
+  grad(j) = grad(j) + lambda / m * theta(j);
+endfor
+
+for i = 1:m
+  h = sigmoid(X(i, :) * theta);
+  grad(1) = grad(1) + (h - y(i)) * X(i, 1);
+endfor
+grad(1) = 1 / m * grad(1);
 
 
 % =============================================================
